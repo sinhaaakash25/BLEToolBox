7c99c459a2f1fc6a81f8878f7b9565f4386ab90c
aakash sinha <aksinha2509@gmail.com>
2022-02-09 14:12:39 +0530

    Initial Commit

---
+++ BLEToolBox/BLEToolBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLEToolBoxApp.swift
+//  BLEToolBox
+//
+//  Created by Aakash Sinha on 09/02/22.
+//
+
+import SwiftUI
+
+@main
+struct BLEToolBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
